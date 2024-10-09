2ed74c35365e51372555a8aeb8875fad8538540e
pswierzy <pswierzy@student.agh.edu.pl>
2024-10-09 19:45:44 +0200

    Lab1 drugi commit (polecenie 15)

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,8 +1,31 @@
 package agh.ics.oop;
 
 public class World {
+    public static void run(String[] args){
+
+        int leng = args.length;
+
+        /*for (int i=0; i<leng-1; i++) {
+            System.out.print(args[i]+", ");
+        }
+        System.out.println(args[leng-1]);*/
+
+        for(String arg : args){
+            switch(arg){
+                case "f" -> System.out.println("Zwierzak idzie do przodu");
+                case "b" -> System.out.println("Zwierzak idzie do tylu");
+                case "r" -> System.out.println("Zwierzak skreca w prawo");
+                case "l" -> System.out.println("Zwierzak skreca w lewo");
+            }
+        }
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("system wystartował");
-        System.out.println("system zakończył działanie");
+        System.out.println("Start");
+
+        run(args);
+
+        System.out.println("Stop");
     }
 }
